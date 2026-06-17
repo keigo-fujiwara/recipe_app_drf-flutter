41df3f577ed6fa2d1abea5999e74d3c74aac2ca0
keigo-fujiwara <keigo_fujiwara@waotech.jp>
2026-06-17 16:10:07 +0900

    finish lesson9

--- recipe_app_flutter/lib/main.dart
+++ recipe_app_flutter/lib/main.dart
@@ -23,7 +23,38 @@ class MyApp extends StatelessWidget {
         ),
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
           backgroundColor: Colors.orange,
-        ), // 追加
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.orange,
+          selectionColor: Colors.orangeAccent,
+          selectionHandleColor: Colors.orange,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.orange,
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          floatingLabelStyle: TextStyle(color: Colors.orange),
+          hintStyle: TextStyle(color: Colors.grey, fontStyle: FontStyle.italic),
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.orange, width: 2.0),
+          ),
+        ),
+        sliderTheme: SliderThemeData(
+          activeTrackColor: Colors.orange,
+          inactiveTrackColor: Colors.orange[100],
+          thumbColor: Colors.orange,
+          overlayColor: Colors.orange.withValues(alpha: 0.2),
+          valueIndicatorColor: Colors.orange,
+          valueIndicatorTextStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       home: const CategoryListScreen(), // 修正
     );
